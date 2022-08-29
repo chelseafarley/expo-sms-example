fa168fe63e0c8aeef5e2d439884adb2c74f04966
Chelsea <chelsea@tripwiretech.com>
2022-08-29 12:23:48 +1200

    Send sms

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as SMS from 'expo-sms';
+
+// expo install expo-sms
 
 export default function App() {
+  const [isAvailable, setIsAvailable] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState(undefined);
+  const [recipients, setRecipients] = useState([]);
+  const [message, setMessage] = useState(undefined);
+
+  useEffect(() => {
+    async function checkAvailability() {
+      const isSmsAvailable = await SMS.isAvailableAsync();
+      setIsAvailable(isSmsAvailable);
+    }
+    checkAvailability();
+  }, []);
+
+  const sendSms = async () => {
+    const {result} = await SMS.sendSMSAsync(
+      recipients,
+      message
+    );
+
+    console.log(result);
+  };
+
+  const addNumber = () => {
+    let newRecipients = [...recipients];
+    newRecipients.push(phoneNumber);
+    setRecipients(newRecipients);
+    setPhoneNumber(undefined);
+  };
+
+  const showRecipients = () => {
+    if (recipients.length === 0) {
+      return <Text>No recipients added!</Text>
+    }
+
+    return recipients.map((recipient, index) => {
+      return <Text key={index}>{recipient}</Text>;
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput value={phoneNumber} placeholder="Phone Number" onChangeText={(value) => setPhoneNumber(value)} />
+      <Button title='Add Number' onPress={addNumber} />
+      <TextInput value={message} placeholder="Message" onChangeText={(value) => setMessage(value)} />
+      <Text>Recipients:</Text>
+      {showRecipients()}
+      <Button title='Clear Recipients' onPress={() => setRecipients([])} />
+      {isAvailable ? <Button title="Send SMS" onPress={sendSms} /> : <Text>SMS not available</Text>}
       <StatusBar style="auto" />
     </View>
   );
